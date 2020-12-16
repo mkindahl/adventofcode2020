786288ac7c2a084946c9874cc30aa37d78808fc0
Mats Kindahl <mats.kindahl@gmail.com>
2020-12-16 21:00:12 +0100

    Day 15: Rambunctious Recitation

---
+++ src/aoc15.rs
@@ -0,0 +1,35 @@
+use std::{collections::HashMap, error::Error};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let numbers = [6, 3, 15, 13, 1, 0];
+    println!("Part 1: {:?}", play(&numbers, 2020));
+    println!("Part 2: {:?}", play(&numbers, 30000000));
+    Ok(())
+}
+
+fn play(numbers: &[usize], nth: usize) -> usize {
+    let mut record: HashMap<_, _> = numbers.iter().enumerate().map(|(x, &y)| (y, x)).collect();
+    let mut spoken = numbers[numbers.len() - 1];
+    for turn in (numbers.len() - 1)..(nth - 1) {
+        if turn % 100000 == 0 {
+            println!("{}", turn);
+        }
+        spoken = match record.insert(spoken, turn) {
+            None => 0,
+            Some(last) => turn - last,
+        };
+    }
+    spoken
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        assert_eq!(play(&[0, 3, 6], 2020), 436);
+        assert_eq!(play(&[1, 3, 2], 2020), 1);
+        assert_eq!(play(&[2, 1, 3], 2020), 10);
+    }
+}
