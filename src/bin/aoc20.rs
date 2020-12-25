6c5991b8f4abe7762d3345ea60512d22b14e3d0c
Mats Kindahl <mats.kindahl@gmail.com>
2020-12-25 09:15:54 +0100

    Adding to day 20

--- src/bin/aoc20.rs
+++ src/bin/aoc20.rs
@@ -1,20 +1,49 @@
 #![feature(str_split_once)]
 
 use regex::Regex;
-use std::{
-    collections::HashMap, error::Error, fs::read_to_string, num::ParseIntError, path::PathBuf,
-};
+use std::{collections::HashMap, fs::read_to_string, path::PathBuf};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     path.push("src/bin/aoc19.dat");
     let contents = read_to_string(path)?;
-    let parts = contents.split_once("\n\n").unwrap();
-    let tiles: Result<HashMap<usize, Body>, _> =
-        parts.0.lines().map(|line| from_str(line)).collect();
-    let rules = rules.unwrap();
-    let messages: Vec<&str> = parts.1.lines().collect();
-    println!("Part 1: {:?}", part1(&rules, &messages));
-    println!("Part 2: {:?}", part2(&rules, &messages));
+    let regex = Regex::new(r"Tile (\d+):").unwrap();
+    let tiles: Result<Vec<Tile>, Error> = contents
+        .split("\n\n")
+        .map(|para| parse_tile(&regex, para.lines().collect()))
+        .collect();
+
+    println!("Part 1: {:?}", part1());
+    println!("Part 2: {:?}", part2());
     Ok(())
 }
+
+struct Tile {
+    number: usize,
+    data: HashMap<(usize, usize), char>,
+}
+
+fn parse_tile(regex: &Regex, lines: Vec<&str>) -> Result<Tile, Error> {
+    if let Some(cap) = regex.captures(lines[0]) {
+        let number = cap.get(1).unwrap().as_str().parse().unwrap();
+        let data: HashMap<(usize, usize), char> = lines[1..]
+            .iter()
+            .enumerate()
+            .flat_map(|(row, line)| {
+                line.chars()
+                    .enumerate()
+                    .map(|(col, ch)| ((row, col), ch))
+                    .collect::<Vec<((usize, usize), char)>>()
+            })
+            .collect();
+        Ok(Tile { number, data })
+    } else {
+        Err(Error)
+    }
+}
+
+fn part1() {}
+
+fn part2() {}
+
+struct Error;
